4b78d6d740a44321bf8c07eb27d901cfc2b49a03
unknown <aechartmon2@educacion.navarra.es>
2026-01-19 10:38:35 +0100

    ejercicio 4 hecho

--- src/ex04_best_student.py
+++ src/ex04_best_student.py
@@ -13,4 +13,9 @@ def best_student(records: list[tuple[str, float]]) -> tuple[str, float]:
     Ejemplo:
     [("Ana", 7.5), ("Luis", 9.0), ("Marta", 8.0)] -> ("Luis", 9.0)
     """
-    raise NotImplementedError("Implementa best_student(records)")
+    if not records:
+        raise  ValueError("La lista de registros está vacía")
+    return max(records, key= lambda record: record[1])
+
+alumnos = [("Ana", 7.5), ("Luis", 9.0), ("Marta", 8.0)]
+print(best_student(alumnos))
